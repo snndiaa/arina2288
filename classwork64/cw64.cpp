ab671e30f8cfa616519c0f26986a935f48ed7699
snndiaa <snndiaaa@gmail.com>
2025-05-28 13:13:21 +0300

    added arina228

---
+++ classwork64/cw64.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	
+	cout << "privet" << endl;
+	//arina228
+	//arina 228
+	cout << "privet" << endl;
+	cout << "poka" << endl;
+	return 0;
+}
