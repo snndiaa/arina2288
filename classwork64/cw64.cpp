856b684020956586f5c13c0edb60ce92c5dce8dc
snndiaa <snndiaaa@gmail.com>
2025-05-28 13:14:39 +0300

    added diana241

--- classwork64/cw64.cpp
+++ classwork64/cw64.cpp
@@ -3,11 +3,12 @@ using namespace std;
 
 int main()
 {
-	
+	//diana241
 	cout << "privet" << endl;
 	//arina228
 	//arina 228
 	cout << "privet" << endl;
 	cout << "poka" << endl;
+	cout << "gavgav" << endl;
 	return 0;
 }
